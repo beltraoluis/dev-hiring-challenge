91c764d4ff42368dbcd6226669470bed047630eb
Luís Henrique Beltrão Santana <beltraoluis@gmail.com>
2019-11-05 03:57:16 -0300

    correção script de inicialização

--- server.js
+++ server.js
@@ -2,13 +2,13 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/dist/dev-hiring-challenge'));
 app.listen(process.env.PORT || 8080);
 
 // PathLocationStrategy
 
 app.get('*', function (req, res) {
-  const index = path.join(__dirname, 'build', 'index.html');
+  const index = path.join(__dirname + '/dist/dev-hiring-challenge/index.html');
   res.sendFile(index);
 });
 
