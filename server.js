39216069a7bfcbadc9a6e6a1e4b9405432e3855c
Luís Henrique Beltrão Santana <beltraoluis@gmail.com>
2019-11-05 03:39:46 -0300

    correção do script de inicialização

--- server.js
+++ server.js
@@ -7,8 +7,9 @@ app.listen(process.env.PORT || 8080);
 
 // PathLocationStrategy
 
-app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/index.html'));
-})
+app.get('*', function (req, res) {
+  const index = path.join(__dirname, 'build', 'index.html');
+  res.sendFile(index);
+});
 
 console.log('Server running!')
